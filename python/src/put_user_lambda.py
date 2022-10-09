1cf12b49f1f50010555e35a097ba6403cf4828ef
ksaittis <ksaittis@gmail.com>
2022-10-09 10:47:14 +0100

    Resolving issue with parsing dynamodb response status code

--- python/src/put_user_lambda.py
+++ python/src/put_user_lambda.py
@@ -49,14 +49,14 @@ def put_user(event, context):
                 }
             )
 
-            if 'HTTPStatusCode' in dynamodb_put_item_response and dynamodb_put_item_response['HTTPStatusCode'] == '200':
+            if dynamodb_put_item_response['ResponseMetadata']['HTTPStatusCode'] == 200:
                 return {
                     'statusCode': 204,
                     'body': ""
                 }
         return {
             'statusCode': 400,
-            'body': json.dumps(f'Bad Request')
+            'body': json.dumps(f'Bad Request!')
         }
 
     except KeyError as key_error:
@@ -74,3 +74,80 @@ def put_user(event, context):
             'statusCode': 500,
             'body': json.dumps('Internal Server Error')
         }
+
+
+if __name__ == '__main__':
+    a = {
+        "resource": "/hello/{username}",
+        "path": "/hello/giannis",
+        "httpMethod": "PUT",
+        "headers": {
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Cache-Control": "no-cache",
+            "Content-Type": "text/plain",
+            "Host": "8x16jn9bu7.execute-api.eu-west-1.amazonaws.com",
+            "Postman-Token": "a43d67d7-4615-4af0-9983-b7e0a1ac9dd7",
+            "User-Agent": "PostmanRuntime/7.29.2",
+            "X-Amzn-Trace-Id": "Root=1-63429571-64b78dd6357341ea6ba4255c",
+            "X-Forwarded-For": "54.86.50.139",
+            "X-Forwarded-Port": "443",
+            "X-Forwarded-Proto": "https"
+        },
+        "multiValueHeaders": {
+            "Accept": ["*/*"],
+            "Accept-Encoding": ["gzip, deflate, br"],
+            "Cache-Control": ["no-cache"],
+            "Content-Type": ["text/plain"],
+            "Host": ["8x16jn9bu7.execute-api.eu-west-1.amazonaws.com"],
+            "Postman-Token": ["a43d67d7-4615-4af0-9983-b7e0a1ac9dd7"],
+            "User-Agent": ["PostmanRuntime/7.29.2"],
+            "X-Amzn-Trace-Id": ["Root=1-63429571-64b78dd6357341ea6ba4255c"],
+            "X-Forwarded-For": ["54.86.50.139"],
+            "X-Forwarded-Port": ["443"],
+            "X-Forwarded-Proto": ["https"]
+        },
+        "queryStringParameters": "None",
+        "multiValueQueryStringParameters": "None",
+        "pathParameters": {
+            "username": "giannis"
+        },
+        "stageVariables": "None",
+        "requestContext": {
+            "resourceId": "di86x0",
+            "resourcePath": "/hello/{username}",
+            "httpMethod": "PUT",
+            "extendedRequestId": "ZuxJxFU8joEFbRQ=",
+            "requestTime": "09/Oct/2022:09:33:37 +0000",
+            "path": "/v1/hello/giannis",
+            "accountId": "208867598194",
+            "protocol": "HTTP/1.1",
+            "stage": "v1",
+            "domainPrefix": "8x16jn9bu7",
+            "requestTimeEpoch": 1665308017541,
+            "requestId": "fc5c0e4f-01d3-4dd2-80b5-e042236da4d5",
+            "identity": {
+                "cognitoIdentityPoolId": "None",
+                "accountId": "None",
+                "cognitoIdentityId": "None",
+                "caller": "None",
+                "sourceIp": "54.86.50.139",
+                "principalOrgId": "None",
+                "accessKey": "None",
+                "cognitoAuthenticationType": "None",
+                "cognitoAuthenticationProvider": "None",
+                "userArn": "None",
+                "userAgent": "PostmanRuntime/7.29.2",
+                "user": "None"
+            },
+            "domainName": "8x16jn9bu7.execute-api.eu-west-1.amazonaws.com",
+            "apiId": "8x16jn9bu7"
+        },
+        "body": "{\n    \"dateOfBirth\" : \"2020-08-01\"\n}",
+        "isBase64Encoded": "False"
+    }
+    os.environ["REGION"] = "eu-west-1"
+    os.environ["DYNAMODB_TABLE_NAME"] = "Users"
+    os.environ['AWS_PROFILE'] = 'development'
+
+    put_user(a, None)
